953eabb7db02273eab9114c516c64fbde3b1d486
Lcng <longh5296@gmail.com>
2025-04-21 16:26:52 +0700

    Chapter 11: Drag & Drop

---
+++ android/app/src/main/java/com/example/ch11_gestrure/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.ch11_gestrure;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:ch11_gestrure/pages/home.dart';
+import 'package:flutter/material.dart';
+void main()=>runApp(MyApp());
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+
+}
